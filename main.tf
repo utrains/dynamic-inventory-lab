07b7b08e9132d603d1d74d6e117b7664542c76ee
tatahnoellimnyuy <tatahnoellimnyuy@gmail.com>
2023-05-20 01:24:46 +0100

    added main

--- main.tf
+++ main.tf
@@ -1,63 +1,12 @@
-resource "aws_vpc" "my-vpc" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-  tags = {
-    Name = "Demo VPC"
-  }
-}
 
-
-# Create Web Public Subnet
-resource "aws_subnet" "web-subnet" {
-  vpc_id                  = aws_vpc.my-vpc.id
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "Web-1a"
-  }
-}
-
-
-
-
-
-
-# Create Internet Gateway
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.my-vpc.id
-
-  tags = {
-    Name = "Demo IGW"
-  }
-}
-
-# Create Web layber route table
-resource "aws_route_table" "web-rt" {
-  vpc_id = aws_vpc.my-vpc.id
-
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-
-  tags = {
-    Name = "WebRT"
-  }
-}
-
-# Create Web Subnet association with Web route table
-resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.web-subnet.id
-  route_table_id = aws_route_table.web-rt.id
+# create default vpc if one does not exit
+resource "aws_default_vpc" "default_vpc" {
 }
   # Create Web Security Group
 resource "aws_security_group" "web-sg" {
     name        = "Web-SG"
     description = "Allow ssh inbound traffic"
-    vpc_id      = aws_vpc.my-vpc.id
+    vpc_id      = aws_default_vpc.default_vpc.id
   
     ingress {
       description = "ssh from VPC"
